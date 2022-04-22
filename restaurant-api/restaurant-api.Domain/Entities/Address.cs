f8b961ab0229de448c62bc3e3d5a34766e25ff5c
Jakub <jakub.wrona97@gmail.com>
2022-04-22 23:56:01 +0200

    Added basic entities

---
+++ restaurant-api/restaurant-api.Domain/Entities/Address.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace restaurant_api.Domain.Entities
+{
+    public record Address
+    {
+        public int Id { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
+
+        public virtual Restaurant Restaurant { get; set; }
+    }
+}
---
+++ restaurant-api/restaurant-api.Domain/Entities/Dish.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace restaurant_api.Domain.Entities
+{
+    public record Dish
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+        public int RestaurantId { get; set; }
+        public virtual Restaurant Restaurant { get; set; }
+    }
+}
