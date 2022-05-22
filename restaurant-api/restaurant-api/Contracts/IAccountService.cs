14a92eb99b5a77a3679211c0d170ac89dab9922e
Jakub <jakub.wrona97@gmail.com>
2022-05-23 01:13:53 +0200

    Added RegisterUser function

---
+++ restaurant-api/restaurant-api/Contracts/IAccountService.cs
@@ -0,0 +1,10 @@
+﻿using restaurant_api.Domain.DTOs.User;
+using System.Threading.Tasks;
+
+namespace restaurant_api.Contracts
+{
+    public interface IAccountService
+    {
+        void RegisterUser(RegisterUserDto registerUserDto);
+    }
+}
---
+++ restaurant-api/restaurant-api/Services/AccountService.cs
@@ -0,0 +1,30 @@
+﻿using restaurant_api.Contracts;
+using restaurant_api.Domain.DTOs.User;
+using restaurant_api.Domain.Entities;
+using restaurant_api.Infrastructure.Context;
+
+namespace restaurant_api.Services
+{
+    public class AccountService : IAccountService
+    {
+        private readonly RestaurantDbContext _dbContext;
+
+        public AccountService(RestaurantDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void RegisterUser(RegisterUserDto registerUserDto)
+        {
+            var newUser = new User()
+            {
+                Email = registerUserDto.Email,
+                DateBirth = registerUserDto.DateOfBirth,
+                Nationality = registerUserDto.Nationality,
+                RoleId = registerUserDto.RoleId,
+            };
+
+            _dbContext.Users.Add(newUser);
+            _dbContext.SaveChanges();
+        }
+    }
+}
