36ac3fc5e09cb02b8414c03e9372b6bba759f033
Jakub <jakub.wrona97@gmail.com>
2022-05-23 01:06:28 +0200

    Added User model and user controller

---
+++ restaurant-api/restaurant-api/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using restaurant_api.Domain.DTOs.User;
+using System.Threading.Tasks;
+
+namespace restaurant_api.Controllers
+{
+    [Route("api/account")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        public AccountController()
+        {
+
+        }
+
+        [HttpPost("register")]
+        public Task<ActionResult> RegisterUser([FromBody] RegisterUserDto registerUserDto)
+        {
+
+        }
+    }
+}
