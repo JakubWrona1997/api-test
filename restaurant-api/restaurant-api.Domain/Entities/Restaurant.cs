6e87bdd0607b110201de562b66ca64f6839a8f28
Jakub <jakub.wrona97@gmail.com>
2022-06-17 17:03:00 +0200

    migration add

--- restaurant-api/restaurant-api.Domain/Entities/Restaurant.cs
+++ restaurant-api/restaurant-api.Domain/Entities/Restaurant.cs
@@ -15,7 +15,7 @@ namespace restaurant_api.Domain.Entities
         public bool HasDelivery { get; set; }
         public string ContactEmail { get; set; }
         public string ContactNumber { get; set; }
-        public int CreatedById { get; set; }
+        public int? CreatedById { get; set; }
         public virtual User CreatedBy { get; set; }
 
         public int AddressId { get; set; }
