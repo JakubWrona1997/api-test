660b0ccb53d2221e9b6d439afbea454d292a4f20
Jakub <jakub.wrona97@gmail.com>
2022-06-19 14:15:26 +0200

    Added authrule

---
+++ restaurant-api/restaurant-api/Authorization/CreatedMultipleRestaurantsRequirement.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace restaurant_api.Authorization
+{
+    public class CreatedMultipleRestaurantsRequirement : IAuthorizationRequirement
+    {
+        public CreatedMultipleRestaurantsRequirement(int minimumRestaurantsCreated)
+        {
+            MinimumRestaurantsCreated = minimumRestaurantsCreated;
+        }
+        public int MinimumRestaurantsCreated { get; }
+    }
+}
